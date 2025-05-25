4ade7796a10188b20a21aefc5594be474ba5e0e8
Mikecodec <michaelfloresrojas31@gmail.com>
2025-05-25 19:20:05 -0400

    up portfolio, sin radio

--- Radio/Estructura-reproductor-master/assets/js/app.js
+++
@@ -1,9 +0,0 @@
-const btnReaccion=document.getElementById('reaccion');
-const contenedorListaMusic=document.getElementById('lista-music');
-const controles=document.getElementById('controles');
-
-const menuMusic=document.getElementById('menuMusic');
-const titlePlaylist=document.getElementById('titlePlaylist');
-const playDescription=document.getElementById('playDescription');
-const imgAlbum=document.getElementById('imgAlbum');
-const album=document.getElementById('album');
